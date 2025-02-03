bed3c69a31953169a088010617733748d1470afb
sravyam521 <sravyamnss@gmail.com>
2025-02-03 20:41:31 +0530

    db.js

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 
-require('dotenv').config(); // Load environment variables from the Render environment
+
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    uri: process.env.MYSQL_URL,
-    host: process.env.MYSQLHOST,       // Host from Render
-    user: 'root',                      // User is usually 'root' for MySQL
-    password: process.env.MYSQLPASSWORD, // Password from Render
-    database: process.env.MYSQLDATABASE, // Database name from Render
-    port: 13395                     // Default MySQL port
+    host: '127.0.0.1',   // Use localhost or the IP address of your server
+    user: 'root',        // Replace with your MySQL username
+    password: '3503',    // Replace with your MySQL password
+    database: 'audiobook', // Replace with your database name
+    port: 3306           // Default MySQL port
 });
 
 module.exports = pool.promise();
+
