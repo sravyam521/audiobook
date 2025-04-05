b230ed97a4f9c697ddc2c9ca93d60622400553ea
sravyam521 <sravyamnss@gmail.com>
2025-04-05 22:39:54 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     uri: process.env.MYSQL_URL,
     host: process.env.MYSQLHOST,       // Host from Render
-    user: 'root',                      // User is usually 'root' for MySQL
+    user: 'avnadmin',                      // User is usually 'root' for MySQL
     password: process.env.MYSQLPASSWORD, // Password from Render
     database: process.env.MYSQLDATABASE, // Database name from Render
     port: 13395                     // Default MySQL port
